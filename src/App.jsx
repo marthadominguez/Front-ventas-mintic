313854678013ecb93a57527fa46902a1d80e8261
Martha Dominguez <marthadominguezg@gmail.com>
2021-12-22 23:57:41 -0500

    configuración inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './styles/styles.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
