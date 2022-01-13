834bcab2ffbfec566667c41b2dc46203bffb4543
Martha Dominguez <marthadominguezg@gmail.com>
2022-01-13 00:20:22 -0500

    correcciones para despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -17,33 +17,33 @@ import { UserContext } from "context/userContext"
 
 function App() {
 
-  const [ userData, setUserData ] = useState({})
+  const [userData, setUserData] = useState({})
 
   return (
     <Auth0Provider
-    domain="sistema-ventas.us.auth0.com"
-    clientId="QTIh1sIfSTok7zOkD3Y2D9sIAIBEMH2E"
-    // redirectUri='http://localhost:3000/'
-    redirectUri="https://sistema-ventas-mld.herokuapp.com/"
-    audience='api-sistema-ventas'
+      domain="sistema-ventas.us.auth0.com"
+      clientId="QTIh1sIfSTok7zOkD3Y2D9sIAIBEMH2E"
+      // redirectUri='http://localhost:3000/'
+      redirectUri="https://sistema-ventas-mld.herokuapp.com/"
+      audience='api-sistema-ventas'
     >
-       <UserContext.Provider value={{ userData, setUserData }}>
-      <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/" element={<PrivateLayout />}>
-              <Route path="registro-ventas" element={<RegistroVentas />} />
-              <Route path="listado-ventas" element={<ListadoVentas />} />
-              <Route path="registro-productos" element={<RegistroProductos />} />
-              <Route path="listado-productos" element={<ListadoProductos />} />
-              <Route path="listado-usuarios" element={<ListadoUsuarios />} />
-              <Route path="home" element={<Home />} />
-            </Route>
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </div>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <div className="App">
+          <Router>
+            <Routes>
+              <Route path="/" element={<PrivateLayout />}>
+                <Route path="registro-ventas" element={<RegistroVentas />} />
+                <Route path="listado-ventas" element={<ListadoVentas />} />
+                <Route path="registro-productos" element={<RegistroProductos />} />
+                <Route path="listado-productos" element={<ListadoProductos />} />
+                <Route path="listado-usuarios" element={<ListadoUsuarios />} />
+                <Route path="home" element={<Home />} />
+              </Route>
+              <Route path="login" element={<Login />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </div>
       </UserContext.Provider>
     </Auth0Provider>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <p>By Martha L. Domínguez</p>
+            <p>Martha L. Domínguez</p>
             <p>|</p>
             <a target="_blank" rel="noreferrer" className='link' href="https://www.linkedin.com/in/martha-liliana-dom%C3%ADnguez-09b722136"><i className="fab fa-linkedin"></i></a>
             <a target="_blank" rel="noreferrer"className='link' href="https://github.com/marthadominguez"><i className="fab fa-github"></i></a>   
