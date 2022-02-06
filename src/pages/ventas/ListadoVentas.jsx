d17cef8962f60b1ead4f2fb299f558052b9c0dd1
Martha Dominguez <marthadominguezg@gmail.com>
2022-02-06 17:30:39 -0500

    loading state en listado de productos añadido

--- src/pages/ventas/ListadoVentas.jsx
+++ src/pages/ventas/ListadoVentas.jsx
@@ -3,6 +3,7 @@ import { eliminarVenta, obtenerVentas, editarVenta } from "../../utils/api";
 import { toast } from "react-toastify";
 import { Dialog } from "@mui/material";
 import { nanoid } from "nanoid";
+import Loading from "components/Loading";
 
 const ListadoVentas = () => {
   const [ventas, setVentas] = useState([]);
@@ -39,56 +40,56 @@ const ListadoVentas = () => {
   }, [busqueda, ventas]);
 
   return (
-        <>
-          <div className="table_container">
-            <div className="table_header">
-              <h2>Gestión de Ventas</h2>
-              <div className="search_input">
-                <input
-                  value={busqueda}
-                  onChange={(e) => {
-                    setBusqueda(e.target.value);
-                  }}
-                  className="search_text"
-                  type="search"
-                  placeholder="Buscar..."
-                />
-                <span className="material-icons-round search_icon">search</span>
-              </div>
-            </div>
-            <div className="table_canvas">
-              <table className="table">
-                <thead>
-                  <tr className="table_row">
-                    <th className="texto tl">ID</th>
-                    <th className="texto">Nombre Cliente</th>
-                    <th className="texto">Medio de Pago</th>
-                    <th className="texto">Contacto</th>
-                    <th className="texto">Fecha</th>
-                    <th className="texto">Vendedor</th>
-                    <th className="texto">Producto</th>
-                    <th>Cant.</th>
-                    <th className="numero">Vr. Total ($)</th>
-                    <th className="numero">Costo Envío ($)</th>
-                    <th className="texto">Estado</th>
-                    <th className="tr"></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {ventasFiltradas.map((venta) => {
-                    return (
-                      <FilasVentas
-                        venta={venta}
-                        key={nanoid()}
-                        setRefetch={setRefetch}
-                      ></FilasVentas>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+    <>
+      <div className="table_container">
+        <div className="table_header">
+          <h2>Gestión de Ventas</h2>
+          <div className="search_input">
+            <input
+              value={busqueda}
+              onChange={(e) => {
+                setBusqueda(e.target.value);
+              }}
+              className="search_text"
+              type="search"
+              placeholder="Buscar..."
+            />
+            <span className="material-icons-round search_icon">search</span>
           </div>
-        </>
+        </div>
+        <div className="table_canvas">
+          <table className="table">
+            <thead>
+              <tr className="table_row">
+                <th className="texto tl">ID</th>
+                <th className="texto">Nombre Cliente</th>
+                <th className="texto">Medio de Pago</th>
+                <th className="texto">Contacto</th>
+                <th className="texto">Fecha</th>
+                <th className="texto">Vendedor</th>
+                <th className="texto">Producto</th>
+                <th>Cant.</th>
+                <th className="numero">Vr. Total ($)</th>
+                <th className="numero">Costo Envío ($)</th>
+                <th className="texto">Estado</th>
+                <th className="tr"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {ventasFiltradas.map((venta) => {
+                return (
+                  <FilasVentas
+                    venta={venta}
+                    key={nanoid()}
+                    setRefetch={setRefetch}
+                  ></FilasVentas>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 };
 
@@ -112,6 +113,7 @@ const FilasVentas = ({ venta, setRefetch }) => {
   };
 
   return (
+    <> {loading ? (<Loading/>) : (
     <tr>
       <td className="texto tl">{venta._id.slice(18)}</td>
       <td className="texto">{venta.cliente}</td>
@@ -177,7 +179,7 @@ const FilasVentas = ({ venta, setRefetch }) => {
           </button>
         </div>
       </Dialog>
-    </tr>
+    </tr>)}</>
   );
 };
 
@@ -201,6 +203,7 @@ const EstadoVentas = ({ venta }) => {
       EditEstadoVenta();
     }
   }, [estado, venta]);
+
   return (
     <select
       className="custom_input_ventas"
